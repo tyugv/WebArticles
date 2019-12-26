b098d7f04ba21569ba6aa7d3396fec77991dacfa
unknown <milsocol@gmail.com>
2019-12-26 17:25:53 +0300

    увеличила длину статьи

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Article(models.Model):
 	title = models.CharField(max_length=100)
-	description = models.CharField(max_length=1000)
+	description = models.TextField(max_length=1000)
 	imgLink = models.CharField(max_length=200)
 
 	def __str__(self):
